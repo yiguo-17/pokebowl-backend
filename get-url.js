fe89364dd97117db1c4272fc0fe469aefc51f851
yiguo-17 <yiguo.qu@codeimmersives.com>
2020-07-14 14:36:02 -0400

    add function for get-url

--- get-url.js
+++ get-url.js
@@ -1,7 +1,10 @@
 // Your code here.
 // Note that you do NOT need the data for this function!
 
-
+const getUrl = function(row){
+ if(row !== null){return row[1];}
+  else {return null;}
+}
 
 
 // OUR code here.
